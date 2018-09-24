ba605d00c0c073f2e1e96595e5ddd0430b408961
Rob Gallagher <rob.gallagher@shropshire.gov.uk>
2018-09-24 11:45:26 +0000

    x

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManager.Services;
+using ContactManager.Models;
+
+
+namespace ContactManager.Controllers
+{
+
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            contactRepository = new ContactRepository();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+            return response;
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        //    return new Contact[]
+        //      {
+        //new Contact
+        //{
+        //    Id = 1,
+        //    Name = "Glenn Block"
+        //},
+        //new Contact
+        //{
+        //    Id = 2,
+        //    Name = "Dan Roth"
+        //}
+        //      };
+
+        }
+    }
+}
